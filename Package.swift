384d95646762c7cf1cc03406c918edb727c0b4f1
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-05-25 10:11:46 +0200

    Package fixes

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Submissions",
+    products: [
+        .library(name: "Submissions", targets: ["Submissions"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
