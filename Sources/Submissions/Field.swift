baa1504f8e4eed39582314bf87f54e463b32f292
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-06-04 17:05:59 +0200

    WIP on passing in entity to validation

--- Sources/Submissions/Field.swift
+++ Sources/Submissions/Field.swift
@@ -2,7 +2,7 @@ import Validation
 import Vapor
 
 /// Represents a property that can be rendered in an html form and validated on submission.
-public struct Field {
+public struct Field<S: Submittable> {
     /// A label describing this field. Used by Tags to render alongside an input field.
     public let label: String?
 
@@ -12,10 +12,10 @@ public struct Field {
     /// Whether or not values are allowed to be nil
     public let isRequired: Bool
 
-    private let _validate: (ValidationContext, Request) throws -> Future<[ValidationError]>
+    private let _validate: (S?, ValidationContext, Request) throws -> Future<[ValidationError]>
 
     /// A closure that can perform async validation of a value in a validation context on a worker.
-    public typealias Validate<T> = (T?, ValidationContext, Request) throws -> Future<[ValidationError]>
+    public typealias Validate<T> = (T?, S?, ValidationContext, Request) throws -> Future<[ValidationError]>
 
     init<T: CustomStringConvertible>(
         label: String? = nil,
@@ -29,7 +29,7 @@ public struct Field {
         self.label = label
         self.value = value?.description
         self.isRequired = isRequired
-        _validate = { context, req in
+        _validate = { submittable, context, req in
             let validationErrors: [ValidationError]
 
             switch (absentValueStrategy.valueIfPresent(value), context, isRequired) {
@@ -69,9 +69,10 @@ public struct Field {
     /// - Returns: An array of `ValidationError`s in the `Future`.
     /// - Throws: Any non-validation related error
     public func validate(
+        _ submittable: Submittable?,
         inContext context: ValidationContext,
         on req: Request
     ) throws -> Future<[ValidationError]> {
-        return try _validate(context, req)
+        return try _validate(submittable, context, req)
     }
 }
