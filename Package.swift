c73e4989782d968c133f29de1be36db4ddd30c5d
Steffen D. Sommer <steffendsommer@icloud.com>
2018-12-04 12:25:48 +0100

    Remove MutableLeafTagConfig

--- Package.swift
+++ Package.swift
@@ -7,12 +7,11 @@ let package = Package(
         .library(name: "Submissions", targets: ["Submissions"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nodes-vapor/sugar.git", from: "3.0.0-beta"),
         .package(url: "https://github.com/vapor/template-kit.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "Submissions", dependencies: ["Sugar", "TemplateKit", "Vapor"]),
+        .target(name: "Submissions", dependencies: ["TemplateKit", "Vapor"]),
         .testTarget(name: "SubmissionsTests", dependencies: ["Submissions"])
     ]
 )
