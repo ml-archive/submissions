e9fd76cfd9ba577b0d69b352f6012ea0fa50fb5c
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-12-11 20:59:59 +0100

    Add whitespaces

--- Sources/Submissions/Tags/LeafTagConfig+submissions.swift
+++ Sources/Submissions/Tags/LeafTagConfig+submissions.swift
@@ -9,11 +9,11 @@ extension LeafTagConfig {
     /// - Parameter paths: the value containing the leaf paths for the `TnputTag`s.
     public mutating func useSubmissionsTags(paths: TagTemplatePaths = .init()) {
         use([
-            "submissions:email": InputTag(templatePath:paths.emailField),
-            "submissions:password": InputTag(templatePath:paths.passwordField),
-            "submissions:text": InputTag(templatePath:paths.textField),
-            "submissions:hidden": InputTag(templatePath:paths.hiddenField),
-            "submissions:textarea": InputTag(templatePath:paths.textareaField)
+            "submissions:email": InputTag(templatePath: paths.emailField),
+            "submissions:password": InputTag(templatePath: paths.passwordField),
+            "submissions:text": InputTag(templatePath: paths.textField),
+            "submissions:hidden": InputTag(templatePath: paths.hiddenField),
+            "submissions:textarea": InputTag(templatePath: paths.textareaField)
         ])
     }
 }
