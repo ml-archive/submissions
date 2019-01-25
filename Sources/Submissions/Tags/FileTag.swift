86522efdf319d0962cc90fe6c82df1c2bc10d18d
cweinberger <christian.weinberger@me.com>
2019-01-25 15:50:26 +0100

    Add separate tag for file and use new bootstrap `FormFile`

--- Sources/Submissions/Tags/FileTag.swift
+++ Sources/Submissions/Tags/FileTag.swift
@@ -1,6 +1,6 @@
 import TemplateKit
 
-public final class InputTag: TagRenderer {
+public final class FileTag: TagRenderer {
     struct InputData: Encodable {
         let key: String
         let value: String?
@@ -8,10 +8,11 @@ public final class InputTag: TagRenderer {
         let isRequired: Bool
         let errors: [String]
         let hasErrors: Bool
-        let placeholder: String?
         let helpText: String?
+        let accept: String?
+        let multiple: String?
     }
-    
+
     let c: (TagContext, InputData) throws -> Future<TemplateData>
 
     public init(templatePath: String) {
@@ -28,8 +29,9 @@ public final class InputTag: TagRenderer {
     public func render(tag: TagContext) throws -> Future<TemplateData> {
         let data = try tag.submissionsData()
 
-        let placeholder = tag.parameters[safe: 1]?.string
-        let helpText = tag.parameters[safe: 2]?.string
+        let helpText = tag.parameters[safe: 1]?.string
+        let accept = tag.parameters[safe: 2]?.string
+        let multiple = tag.parameters[safe: 3]?.string
 
         let inputData = InputData(
             key: data.key,
@@ -38,8 +40,9 @@ public final class InputTag: TagRenderer {
             isRequired: data.isRequired,
             errors: data.errors,
             hasErrors: data.hasErrors,
-            placeholder: placeholder,
-            helpText: helpText
+            helpText: helpText,
+            accept: accept,
+            multiple: multiple
         )
 
         return try c(tag, inputData)
